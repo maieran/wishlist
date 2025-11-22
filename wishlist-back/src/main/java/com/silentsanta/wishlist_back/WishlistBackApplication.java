65d1325313c4f2d20b1ebc73324f9b045912426f
maieran <apak_mc@hotmail.de>
2025-11-22 15:12:31 +0100

    feat: add spring boot project for backend + gitingore for root, front and back

---
+++ wishlist-back/src/main/java/com/silentsanta/wishlist_back/WishlistBackApplication.java
@@ -0,0 +1,13 @@
+package com.silentsanta.wishlist_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishlistBackApplication.class, args);
+	}
+
+}
---
+++ wishlist-back/src/test/java/com/silentsanta/wishlist_back/WishlistBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silentsanta.wishlist_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
